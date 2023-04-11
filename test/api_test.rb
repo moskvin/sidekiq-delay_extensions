b5f0d5c549a0174431b7e88bc62036ba4c6e4f97
Nikolay Moskvin <nikolay.moskvin@gmail.com>
2023-04-11 19:24:21 +1000

    [test] cleanup v2

--- test/api_test.rb
+++ test/api_test.rb
@@ -5,23 +5,6 @@ require 'sidekiq/api'
 require 'active_job'
 require 'action_mailer'
 
-class ApiMailer < ActionMailer::Base
-  def test_email(*); end
-end
-
-class ApiJob < ActiveJob::Base
-  def perform(*); end
-end
-
-class ApiWorker
-  include Sidekiq::Job
-end
-
-class WorkerWithTags
-  include Sidekiq::Job
-  sidekiq_options tags: ['foo']
-end
-
 describe 'API' do
   before { Sidekiq.redis(&:flushdb) }
 
@@ -29,12 +12,6 @@ describe 'API' do
     before do
       ActiveJob::Base.queue_adapter = :sidekiq
       ActiveJob::Base.logger = nil
-    end
-
-    it 'shows queue as empty' do
-      q = Sidekiq::Queue.new
-      assert_equal 0, q.size
-      assert_equal 0, q.latency
     end
 
     it 'unwraps delayed jobs' do
